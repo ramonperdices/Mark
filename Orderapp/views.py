8a811b6a4180dd98bfd11af4c607aed0a9b7651a
ramonperdices <ramonperdices5@gmail.com>
2019-10-25 23:34:28 +0800

    test
    added:

--- Orderapp/views.py
+++ Orderapp/views.py
@@ -44,6 +44,7 @@ def order(request):
             form = OrderForm()
             context = {
                 'form': form,
+                'alert_flag': True
             }
             return render(request, 'order_form.html', context)
         else:
